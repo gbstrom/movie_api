89636662d21d64bbf78cb401f3a5c38092b30dc6
gbstrom <85531363+gbstrom@users.noreply.github.com>
2021-09-17 14:00:36 -0400

    readded authentication for movies endpoint

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ mongoose.set('useFindAndModify', false);
 
 //return a list of ALL movies to the user
 
-app.get('/movies', /*passport.authenticate('jwt', { session: false }),*/ (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
