14445b20233101623a8bf7732e5f31dc88ea685e
gbstrom <85531363+gbstrom@users.noreply.github.com>
2022-02-10 11:24:05 -0500

    tried to change config again

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
   JWT_SECRET: process.env.JWT_SECRET || 'your_jwt_secret',
   //CONNECTION_URI: process.env.CONNECTION_URI || 'mongodb://localhost:27017/myFlixDB',
-  CONNECTION_URI: 'mongodb+srv://gbstrom:MongoDBWhe!st0ne@gbsdb.9g6xt.mongodb.net/myFlixDB?retryWrites=true&w=majority'
+  CONNECTION_URI: 'mongodb+srv://gbstrom:MongoDBWhe%21st0ne@gbsdb.9g6xt.mongodb.net/myFlixDB?retryWrites=true&w=majority'
 }
 module.exports = config;
